9954d7f4366b2696f93475a67b5f5a9f9ba42a4f
zuberek <maras2132@gmail.com>
2023-01-20 11:39:27 +0100

    dsadas

--- Desktop/krzysztofik_weather_app/src/views/HomeView.vue
+++ Desktop/krzysztofik_weather_app/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div>
+    <div class="search-box">
       <span class="p-input-icon-left">
         <i class="pi pi-search" />
         <InputText
@@ -10,27 +10,34 @@
           placeholder="Search"
         />
       </span>
-      <Button @click="fetchWeather(searchQuery)" label="Submit" />
-      <div class="cards">
-        <div class="card1">
-          <Card style="width: 8rem; margin-bottom: 2em">
-            <template #content>
-              <Button class="btn" label="Basic info" />
-              <Button class="btn" label="Details" />
-              <Button class="btn" label="Submit" />
-            </template>
-          </Card>
-        </div>
-
-        <Card style="width: 25rem; margin-bottom: 2em">
-          <template #title>
-            {{ today }}
-            <p>{{ searchQuery }}</p>
-          </template>
+      <Button @click="currentWeather(searchQuery)" label="Submit" />
+    </div>
+    <div class="cards">
+      <div>
+        <Card style="width: 6rem">
           <template #content>
-            <p>{{ description }}</p>
-            <img class="icon" :src="`/icons/${icon}.svg`" />
-            <p>{{ basicInfo.temp }}℃</p>
+            <div class="card1">
+              <Button class="btn" label="Basic info" />
+              <Button
+                @click="getWeatherForecast(searchQuery)"
+                class="btn"
+                label="Details"
+              />
+              <Button class="btn" label="Submit" />
+            </div>
+          </template>
+        </Card>
+      </div>
+      <div>
+        <Card class="card" style="width: 30rem">
+          <template #content>
+            <div class="flex">
+              {{ today }}
+              <p v-if="loader">{{ searchQuery }}</p>
+              <p>{{ description }}</p>
+              <img v-if="loader" class="icon" :src="`/icons/${icon}.svg`" />
+              <p v-if="loader">{{ basicInfo.temp }}℃</p>
+            </div>
           </template>
         </Card>
       </div>
@@ -38,8 +45,9 @@
   </div>
 </template>
 <script setup>
-import { ref, reactive } from 'vue'
-
+import { ref } from 'vue'
+import axios from 'axios'
+let loader = false
 const now = new Date()
 var months = now.getMonth() + 1
 if (months < 11) {
@@ -49,28 +57,36 @@ const today = now.getDate() + '.' + months + '.' + now.getFullYear()
 
 const searchQuery = ref('')
 const weatherData = ref('')
-const basicInfo = ref('');
+const basicInfo = ref('')
 const description = ref('')
 const icon = ref('')
 
-const fetchWeather = async (query) => {
+const currentWeather = async (query) => {
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&APPID=11f8ef5fb876de2d2394104040969315`
 
   await fetch(url)
     .then((response) => response.json())
-    .then((data) => (weatherData.value = data),
-    console.log(weatherData.value))
+    .then((data) => {
+      weatherData.value = data
+    })
+  loader = true
   description.value = weatherData.value.weather[0].description
   basicInfo.value = weatherData.value.main
   icon.value = weatherData.value.weather[0].icon
-  console.log(basicInfo.value)
+
+  //console.log(basicInfo.value)
 }
 
+const getWeatherForecast = async (query) => {
+  const forecast = `http://api.openweathermap.org/data/2.5/forecast?q=${query}&cnt=8&appid=11f8ef5fb876de2d2394104040969315`
 
+  const weatherForecast = await axios.get(forecast)
+  console.log(weatherForecast.data.list[0].dt_txt)
+}
 </script>
 
 <style scoped>
 .searchbox {
-  width: 450px;
+  width: 520px;
 }
 </style>
